a5e42d29d1ea342244d4efe897dffda221cfa57e
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-25 22:32:31 -0400

    Initialize project using Create React App

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "../../components/Button";
+import { Header } from '../../components/Header'
+
+const Home = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export { Home }
